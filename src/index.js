06677b4e662887509d93b31cc69cb5f711897741
ek0179 <Liz.keith56@gmail.com>
2024-01-18 13:45:29 -0500

    cleaned up JS and changed colors to ###

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-function submitRequest(){ 
-    
-    const standardElement=document.getElementById("standard");
-    if (standardElement.value=== "standard") {
-alert ("Thank you for your buisness. Your letter will be delivered within 3-5 days!");
-}else{
-    alert("Thank you for your buisness. Your letter will be delivered within 1-2 days!");
-    }
+function submitRequest() {
+  const weightInput = document.querySelector("#weight");
+  if (!weightInput.checkValidity()) {
+    alert("Please enter a valid weight.");
+    return;
+  }
+
+  const weight = parseInt(weightInput.value);
+  if (weight > 8) {
+    alert("Thank you for your business. Your letter will be delivered within 3-5 days!");
+  } else {
+    alert("Thank you for your business. Your letter will be delivered within 1-2 days!");
+  }
 }
 
-let button=document.querySelector ("#btn");
-button.addEventListener("click",submitRequest);
+let button = document.querySelector("#btn");
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  submitRequest();
+});
